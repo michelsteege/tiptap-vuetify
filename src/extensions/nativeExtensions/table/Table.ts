10a0489c610f133bba44ae4fb40cab6dda150708
michelsteege <michelsteege@hotmail.com>
2022-01-05 10:50:37 +0100

    Table icon

--- src/extensions/nativeExtensions/table/Table.ts
+++ src/extensions/nativeExtensions/table/Table.ts
@@ -20,7 +20,7 @@ export default class Table extends AbstractExtension {
         render: new ExtensionActionRenderBtn({
           tooltip: 'Table',
           icons: {
-            [VuetifyIconsGroups.md]: new VuetifyIcon('table'),
+            [VuetifyIconsGroups.md]: new VuetifyIcon('table_chart'),
             [VuetifyIconsGroups.fa]: new VuetifyIcon('fas fa-table'),
             [VuetifyIconsGroups.mdi]: new VuetifyIcon('mdi-table'),
             [VuetifyIconsGroups.mdiSvg]: new VuetifyIcon('M8.5,13.5L11,16.5L14.5,12L19,18H5M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19Z')
